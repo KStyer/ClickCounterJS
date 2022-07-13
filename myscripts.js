f77ad3fc0e52452ec6f451f66761ee230e7ba93c
K. Styer <kaitlin@kstyer.com>
2022-07-13 17:03:18 -0500

    created click event

--- myscripts.js
+++ myscripts.js
@@ -3,8 +3,16 @@ window.onload=(e)=>{
     window.alert(message)
     let message2=sendGreetingTo("Koto", "lets go fly a kite!")
     console.log(message2)
+    let clickTargetElement = document.getElementById("clickTarget")
+    clickTargetElement.addEventListener("click", iterateCounter())
 }
 
 function sendGreetingTo(to, greeting){
     return "Hello There, " + to + " " + greeting;
+}
+function iterateCounter(){
+    return ()=>{
+        console.log("click event handled")
+    }
+    
 }
