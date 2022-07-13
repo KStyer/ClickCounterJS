12b48ee816307dcca51eb6883549654268bd5448
K. Styer <kaitlin@kstyer.com>
2022-07-12 19:12:27 -0500

    adjusted popup

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,10 @@
-window.onload=()=>{
-    window.alert("Hello There")
+window.onload=(e)=>{
+    let message=sendGreetingTo("Crumbsby", "want a snack?")
+    window.alert(message)
+    let message2=sendGreetingTo("Koto", "lets go fly a kite!")
+    console.log(message2)
+}
+
+function sendGreetingTo(to, greeting){
+    return "Hello There, " + to + " " + greeting;
 }
