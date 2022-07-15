21cd3c0078b3fa9fedf3e682feaeb54ae4ab5054
K. Styer <kaitlin@kstyer.com>
2022-07-15 02:44:47 -0500

    added click counter

--- myscripts.js
+++ myscripts.js
@@ -1,10 +1,12 @@
 window.onload=(e)=>{
-    let message=sendGreetingTo("Crumbsby", "want a snack?")
-    window.alert(message)
-    let message2=sendGreetingTo("Koto", "lets go fly a kite!")
-    console.log(message2)
-    let clickTargetElement = document.getElementById("clickTarget")
-    clickTargetElement.addEventListener("click", iterateCounter())
+    // let message=sendGreetingTo("Crumbsby", "want a snack?")
+    // window.alert(message)
+    // let message2
+    // message2=sendGreetingTo("Koto", "lets go fly a kite!")
+    // console.log(message2)
+    let clickTargetElement = document.getElementById("clickTarget");
+    clickTargetElement.addEventListener("click", iterateCounter());
+
 }
 
 function sendGreetingTo(to, greeting){
@@ -12,7 +14,12 @@ function sendGreetingTo(to, greeting){
 }
 function iterateCounter(){
     return ()=>{
-        console.log("click event handled")
+        console.log("click event handled");
+        let counterElement = document.getElementById("numberOfClicks");
+        let currentCount = parseInt(counterElement.innerText);
+        let newCount = currentCount + 1;
+        counterElement.innerText = newCount;
+        counterElement.appendChild
     }
     
 }
