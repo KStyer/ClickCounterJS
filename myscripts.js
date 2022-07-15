75775310724a795da1a4e27457cd034b00f14624
K. Styer <kaitlin@kstyer.com>
2022-07-15 03:04:52 -0500

    added click counter subtractor

--- myscripts.js
+++ myscripts.js
@@ -1,4 +1,4 @@
-window.onload=(e)=>{
+window.onload = (e) => {
     // let message=sendGreetingTo("Crumbsby", "want a snack?")
     // window.alert(message)
     // let message2
@@ -7,13 +7,16 @@ window.onload=(e)=>{
     let clickTargetElement = document.getElementById("clickTarget");
     clickTargetElement.addEventListener("click", iterateCounter());
 
+    let clickTargetElementLess = document.getElementById("clickTarget2");
+    clickTargetElementLess.addEventListener("click", iterateCounter2());
+
 }
 
-function sendGreetingTo(to, greeting){
+function sendGreetingTo(to, greeting) {
     return "Hello There, " + to + " " + greeting;
 }
-function iterateCounter(){
-    return ()=>{
+function iterateCounter() {
+    return () => {
         console.log("click event handled");
         let counterElement = document.getElementById("numberOfClicks");
         let currentCount = parseInt(counterElement.innerText);
@@ -21,5 +24,15 @@ function iterateCounter(){
         counterElement.innerText = newCount;
         counterElement.appendChild
     }
-    
+}
+
+function iterateCounter2() {
+    return () => {
+        console.log("less click event handled");
+        let counterElement = document.getElementById("numberOfClicks");
+        let currentCount = parseInt(counterElement.innerText);
+        let newCount = currentCount - 1;
+        counterElement.innerText = newCount;
+        counterElement.appendChild;
+    }
 }
