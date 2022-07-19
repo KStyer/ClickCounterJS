6a1fbed338209ae72f52f63153c4e8eea6eac6a2
K. Styer <kaitlin@kstyer.com>
2022-07-19 16:18:42 -0500

    added navigation arrows and arranged visually using table

--- myscripts.js
+++ myscripts.js
@@ -10,6 +10,18 @@ window.onload = (e) => {
     let clickTargetElementLess = document.getElementById("clickTarget2");
     clickTargetElementLess.addEventListener("click", decrementCounter());
 
+    let moveUpButton = document.getElementById("moveUp");
+    moveUpButton.addEventListener("click", upAction());
+
+    let moveDownButton = document.getElementById("moveDown");
+    moveDownButton.addEventListener("click", downAction());
+
+    let moveLeftButton = document.getElementById("moveLeft");
+    moveLeftButton.addEventListener("click", leftAction());
+
+    let moveRightButton = document.getElementById("moveRight");
+    moveRightButton.addEventListener("click", rightAction());
+
 }
 
 function sendGreetingTo(to, greeting) {
@@ -34,5 +46,50 @@ function changeCounterValueBy(changeAmount) {
     let currentCount = parseInt(counterElement.innerText);
     let newCount = currentCount + changeAmount;
     counterElement.innerText = newCount;
-    counterElement.appendChild;
+    // counterElement.appendChild;
+}
+
+function upAction() {
+    return () => {
+        let circleElement = document.getElementById("blueCircle");
+        // let currentTopMarginString = circleElement.style.marginTop;
+        let currentTopMargin = parseInt(circleElement.style.marginTop);
+        // currentTopMargin = parseInt(currentTopMarginString);
+        let newTopMargin = currentTopMargin -5;
+        console.log("circle moved from " + currentTopMargin + " to " + newTopMargin);
+        circleElement.style.marginTop = newTopMargin;
+    }
+}
+
+function downAction() {
+    return () => {
+        let circleElement = document.getElementById("blueCircle");
+        let currentBottomMargin = parseInt(circleElement.style.marginTop);
+        let newBottomMargin = currentBottomMargin +5;
+        console.log("circle moved from " + currentBottomMargin + " to " + newBottomMargin);
+        circleElement.style.marginTop = newBottomMargin;
+    }
+
+}
+
+function leftAction() {
+    return () => {
+        let circleElement = document.getElementById("blueCircle");
+        let currentLeftMargin = parseInt(circleElement.style.marginLeft);
+        let newLeftMargin = currentLeftMargin -5;
+        console.log("circle moved from " + currentLeftMargin + " to " + newLeftMargin);
+        circleElement.style.marginLeft = newLeftMargin;
+    }
+
+}
+
+function rightAction() {
+    return () => {
+        let circleElement = document.getElementById("blueCircle");
+        let currentRightMargin = parseInt(circleElement.style.marginLeft);
+        let newRightMargin = currentRightMargin +5;
+        console.log("circle moved from " + currentRightMargin + " to " + newRightMargin);
+        circleElement.style.marginLeft = newRightMargin;
+    }
+
 }
